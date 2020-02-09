bfe83f9d46370f7f26445626a3eeba65947e636a
Rohan Padmanabhan <rohan@padmanabhan.net>
2020-02-09 00:17:09 +0000

    Add state management code

--- listener.py
+++ listener.py
@@ -1,13 +1,62 @@
 import sys
-
-def checkStatus(lookingFor, line):
-    if lookingFor in line:
-        print(lookingFor, flush=True)
+from enum import Enum
 
 
-for line in sys.stdin:
-    checkStatus('Connecting...', line)
-    checkStatus('Listening...', line)
-    checkStatus('Thinking...', line)
-    checkStatus('Speaking...', line)
-    checkStatus('Alexa is currently idle!', line)
+class AlexaState(Enum):
+    CONNECTING = 'Connecting...'
+    IDLE = 'Alexa is currently idle!'
+    LISTENING = 'Listening...'
+    THINKING = 'Thinking...'
+    SPEAKING = 'Speaking...'
+
+        
+def parseStatus(line):
+    for state in AlexaState:
+        if state.value in line:
+            return state
+
+def raiseHead():
+    print("Raising head")
+    
+def lowerHead():
+    print("Lower head")
+
+def turnLedsOn():
+    print("Turn LEDs on")
+
+def turnLedsOff():
+    print("Turn LEDs off")
+    
+def moveMouth():
+    print("Move mouth")
+    
+def shutMouth():
+    print("Stop move mouth")
+
+def main():
+    currentState = AlexaState.CONNECTING
+    for line in sys.stdin:
+        
+        newState = parseStatus(line)
+        
+        if newState is None or newState == currentState:
+            continue
+        
+        currentState = newState
+        
+        if currentState == AlexaState.LISTENING:
+            turnLedsOn()
+            raiseHead()
+            
+        if currentState == AlexaState.SPEAKING:
+            moveMouth()
+        
+        if currentState == AlexaState.IDLE:
+            shutMouth()
+            lowerHead()
+            turnLedsOff()
+
+
+if __name__ == '__main__':
+    main()
+        
